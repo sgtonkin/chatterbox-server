d545e68dfa50a60ccc70ceb267c6c68f11f629f2
Hack Reactor Students <accounts@hackreactor.com>
2014-11-18 15:47:43 -0800

    Began refactoring / code cleanup

--- request-handler.js
+++ request-handler.js
@@ -1,22 +1,19 @@
-var defaultCorsHeaders = {
+var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "text/plain",
 };
-/*************************************************************
 
-You should implement your request handler function in this file.
+var sendResponse = function (response, statusCode, contentType, payload) {
+  var statusCode = statusCode || 200;
+  var payload = payload || "{}";
+  headers['Content-Type'] = contentType;
+  response.writeHead(statusCode, headers);
+  return response.end(JSON.stringify(payload));
+}
 
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 var url = require("url");
 var fs = require("fs");
 var crypto = require("crypto");
@@ -44,20 +41,7 @@ fs.open("./client/index.html", 'rs', function(err, fd) {
 });
 
 exports.requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   // DEFINE DEFAULTS
@@ -145,13 +129,5 @@ exports.requestHandler = function(request, response) {
 
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
+
 
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-/* Import node's http module: */
 var http = require("http");
 var requestHandler = require("./request-handler.js");
-
 
 // SERVER SETTINGS
 var port = 80;
@@ -11,31 +9,7 @@ var ip = "100.73.116.191";
 // var port = 3000;
 // var ip = "localhost";
 
-
-
-
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
 var server = http.createServer(requestHandler.requestHandler);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
-
-
-
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
 
