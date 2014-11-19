a617a773254dc5bc3f1bc826d851e24815beccc6
Hack Reactor Students <accounts@hackreactor.com>
2014-11-18 18:54:40 -0800

    updated paths

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var http = require("http");
 var requestHandler = require("./request-handler.js");
 
 // SERVER SETTINGS
-// var port = 80;
-// var ip = "100.73.116.191";
+var port = 80;
+var ip = "100.73.116.191";
 
 // LOCAL SETTING
-var port = 3000;
-var ip = "localhost";
+// var port = 3000;
+// var ip = "localhost";
 
 var server = http.createServer(requestHandler.requestHandler);
 console.log("Listening on http://" + ip + ":" + port);
